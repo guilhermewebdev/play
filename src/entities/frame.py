3b2435d3aacbc10ca056d94cf02143588dbb4558
Guilherme Isaías <guilherme@guilhermeweb.dev>
2022-05-07 01:51:42 -0300

    feat: render frame to ascii

--- src/entities/frame.py
+++ src/entities/frame.py
@@ -1,6 +1,29 @@
-class Frame:
-    def __init__(self, frame):
-        self.frame = frame
+from PIL import Image
 
-    def render():
-        pass
+class Frame:
+    chars = ["B","S","#","&","@","$","%","*","!",":","."]
+    
+    def __init__(self, frame, size):
+        self.frame = frame
+        image = Image.fromarray(frame)
+        width, height = image.size
+        aspect_ratio = height/width
+        self.size = size
+        new_height = aspect_ratio * self.size * 0.55
+        self.image = image.resize((self.size, int(new_height)))
+        self.image = self.image.convert('L')
+        self.pixels = self.image.getdata()
+
+    def generate_image(self):
+        new_pixels = [self.chars[pixel//25] for pixel in self.pixels]
+        new_pixels = ''.join(new_pixels)
+        new_pixels_count = len(new_pixels)
+        ascii_image = [new_pixels[
+            index:index + self.size] for index in range(0, new_pixels_count, self.size)
+        ]
+        ascii_image = "\n".join(ascii_image)
+        return ascii_image
+
+    def render(self):
+        rendered = self.generate_image()
+        print(rendered)
---
+++ src/entities/test_frame.py
@@ -0,0 +1,22 @@
+import cv2
+from frame import Frame
+
+
+def render_frame(image):
+    frame = Frame(image, 120)
+    rendered = frame.generate_image()
+    assert isinstance(rendered, str)
+
+def test_frame():
+    video = cv2.VideoCapture("fake/frame.png")
+    assert video.isOpened()
+    while(video.isOpened()):
+        ret, frame = video.read()
+        if ret == True:
+            render_frame(frame)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    video.release()
+    cv2.destroyAllWindows()
