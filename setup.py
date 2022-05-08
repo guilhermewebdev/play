f083903c30ed2f621e8bb08f20310ff30607b1e6
Guilherme Isaías <guilherme@guilhermeweb.dev>
2022-05-08 15:36:04 -0300

    fix: version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='play-video',
-    version='1.0.4',
+    version='1.0.5',
     description='Play video on terminal',
     author='Guilherme Isaías',
     author_email='guilherme@guilhermeweb.dev',
