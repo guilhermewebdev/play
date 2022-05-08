1c99b1425a0967a88341c1e03cdb7cb2e30c8c8f
Guilherme Isaías <guilherme@guilhermeweb.dev>
2022-05-08 14:16:43 -0300

    feat: setup Pypi publishing

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from distutils.core import setup
+import setuptools
 
 setup(
     name='Play',
@@ -9,12 +10,24 @@ setup(
     author='Guilherme Isaías',
     author_email='guilherme@guilhermeweb.dev',
     url='https://github.com/guilhermewebdev/play',
+    description_file='README.md',
+    license='GNU General Public License v3.0',
+    platforms=['Linux', 'MacOS', 'Windows'],
     install_requires=[
         'opencv-python',
         'Pillow',
     ],
-    packages=['lib', 'lib.cli', 'lib.entities', 'lib.controllers'],
+    packages=setuptools.find_packages(exclude=['tests']),
     entry_points={
         'console_scripts': ['play=lib.cli:play']
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Topic :: Multimedia :: Video :: Display",
+        "Framework :: Pytest",
+    ],
+    python_requires='>=3.7',
 )
