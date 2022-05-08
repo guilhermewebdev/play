d77c7aa9b4456171435082c6ab9c88a1c7266123
Guilherme Isaías <guilherme@guilhermeweb.dev>
2022-05-08 10:22:17 -0300

    chore: github actions setup

--- setup.py
+++ setup.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python3
 
 from distutils.core import setup
-from importlib.metadata import entry_points
-from pathlib import Path
-import pkg_resources
-
-requirements_path = Path(__file__).parent / "requirements.txt"
-
-with requirements_path.open() as requirements_txt:
-    install_requires = [
-        str(requirement)
-        for requirement
-        in pkg_resources.parse_requirements(requirements_txt)
-    ]
 
 setup(
     name='Play',
@@ -21,7 +9,10 @@ setup(
     author='Guilherme Isaías',
     author_email='guilherme@guilhermeweb.dev',
     url='https://github.com/guilhermewebdev/play',
-    install_requires=install_requires,
+    install_requires=[
+        'opencv-python',
+        'Pillow',
+    ],
     packages=['lib', 'lib.cli', 'lib.entities', 'lib.controllers'],
     entry_points={
         'console_scripts': ['play=lib.cli:play']
